3bb3ce4bc3ef31493c23e955b538d907e56e8834
Jens Kötschau <123948185+Jenseko@users.noreply.github.com>
2024-12-11 17:09:37 +0100

    setting up supabase, create some tables and frontend implementation

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://vzrruoztwufdcalwudgd.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6cnJ1b3p0d3VmZGNhbHd1ZGdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM5Mjc4MDMsImV4cCI6MjA0OTUwMzgwM30.UjkC-IfgNJ4mJaamrVYg56AwAHquP3dgNN-645xe9lg";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
